e8624d7eaabbba71d9117154e6ab5b31800ca393
Ignacio Herrera <ignacioromanherrera@gmail.com>
2023-12-14 21:52:10 -0300

    Update basePath

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -8,21 +8,21 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  organizationName: "herrera-ignacio",
+  projectName: "cybersecurity-handbook",
+  deploymentBranch: "gh-pages",
   title: 'Cybersecurity Handbook',
   tagline: 'Cybersecurityh handbook',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://herrera-ignacio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cybersecurity-handbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
